8d1b32a80fdf76b2885ba9a81ce78d4927e8ef1a
ddziuba <Dorota.Dziuba@lppsa.com>
2022-05-07 15:06:34 +0200

    Array excercise

---
+++ src/main/java/ArrayExcercise.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayExcercise {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (Integer i = 0; i <= 100; i++ ) {
+            if(i % 2 == 0) {
+                numbers.add(i * 2);
+            } else {
+                numbers.add(i);
+            }
+            System.out.println(numbers.get(i));
+        }
+    }
+}
