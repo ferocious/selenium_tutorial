b4e8d08c48dad191871881d30736a31ab7a0dbaf
ddziuba <Dorota.Dziuba@lppsa.com>
2022-05-07 16:22:32 +0200

    Calculator excercise

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("a + b =" + add(2,5));
+        System.out.println("a - b =" + substract(10,2));
+        System.out.println("a * b =" + multiply(2,5));
+        System.out.println("a / b =" + divide(10,2));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int substract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
